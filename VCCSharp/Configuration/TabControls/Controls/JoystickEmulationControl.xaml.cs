6037f3e0b7453c19acc0d5ba0243a703b0f1b97d
Eric Hood <eric.hood@ansira.com>
2021-03-18 19:23:30 -0500

    I've got a decent mockup of the configuration and its dialogs now.

---
+++ VCCSharp/Configuration/TabControls/Controls/JoystickEmulationControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace VCCSharp.Configuration.TabControls.Controls
+{
+    public partial class JoystickEmulationControl : UserControl
+    {
+        public string Side { get; set; } = "Left";
+
+        public JoystickEmulationControl()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
---
+++ VCCSharp/Configuration/TabControls/Controls/JoystickInputControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace VCCSharp.Configuration.TabControls.Controls
+{
+    public partial class JoystickInputControl : UserControl
+    {
+        public string Side { get; set; } = "Left";
+
+        public JoystickInputControl()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
