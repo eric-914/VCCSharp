ae57d09f426b152f8c6bea933e7e1b41f18b907d
Eric Hood <eric.hood@ansira.com>
2021-05-29 10:57:30 -0500

    Work on resources

--- resources/resource.h
+++ resources/resource.h
@@ -2,42 +2,12 @@
 // Microsoft Visual C++ generated include file.
 // Used by resources.rc
 //
-#define IDS_APP_TITLE                   1
 #define IDS_CATNUMBER                   2
-#define IDAPPLY                         3
 #define IDR_MENU                        101
 #define IDI_COCO3                       103
 #define IDC_NONE                        113
-#define IDD_TCONFIG                     135
-#define IDD_CASSETTE                    157
-#define IDD_NEWTAPE                     158
-#define IDC_CONFIGTAB                   1036
-#define IDC_TCOUNT                      1019
-#define IDC_TAPEFILE                    1020
-#define IDM_HELP_ABOUT                  40003
-#define ID_CARTS_LOAD                   40006
-#define ID_DISKS_DRIVE0                 40007
-#define ID_DISKS_DRIVE1                 40008
-#define ID_DISKS_DRIVE2                 40009
-#define ID_DISKS_DRIVE3                 40010
-#define ID_DISKS_HD                     40010
-#define ID_DUMPMEM                      40012
-#define ID_CONSOLE                      40013
-#define ID_CPUDUMP                      40014
-#define ID_RBUTTON                      40015
-#define ID_LBUTTON                      40016
-#define ID_DISKS_DRIVE0_UNMOUNT         40018
-#define ID_DISKS_DRIVE1_UNMOUNT         40019
-#define ID_DISKS_DRIVE2_UNMOUNT         40020
-#define ID_DISKS_HARDDRIVE_UNMOUNT      40021
-#define ID_QUICK                        40022
-#define ID_IRQ                          40023
-#define ID_FIRQ                         40024
-#define ID_NMI                          40025
-#define ID_CARTRIDGE_INSERT             40026
-#define ID_CARTS_EJECT                  40030
+
 #define ID_PLACEHOLDER                  40031
-#define IDC_REMEMBER_SIZE               40039
 
 // Next default values for new objects
 // 
