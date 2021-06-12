2bb8e1dfd6a9353931c8f6f5e4d7386369bc0a1e
Eric Hood <eric.hood@ansira.com>
2021-06-11 20:26:37 -0500

    Apparently don't need the library resource files at all.

--- library/resource.h
+++
@@ -1,7 +0,0 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by library.rc
-//
-#define IDS_MODULE_NAME                 1
-#define IDS_CATNUMBER                   2
-#define IDS_VERSION                     3
