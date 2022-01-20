cf86ab241bea6aee7351dc66cae6fa400d72671a
Eric Hood <eric.hood@ansira.com>
2022-01-19 22:26:45 -0600

    A few more unsafe dealt with.

--- DX8/Models/MEMBLOCKS.cs
+++ DX8/Models/MEMBLOCKS.cs
@@ -23,33 +23,89 @@ namespace DX8.Models
     [StructLayout(LayoutKind.Explicit, Size = 8, CharSet = CharSet.Ansi)]
     public struct BYTE8
     {
-        public unsafe byte this[int index]
+        public byte this[int index]
         {
-            get => B[index];
-            set => B[index] = value;
+            get
+            {
+                switch (index)
+                {
+                    case 0: return B0;
+                    case 1: return B1;
+                    case 2: return B2;
+                    case 3: return B3;
+                    case 4: return B4;
+                    case 5: return B5;
+                    case 6: return B6;
+                    case 7: return B7;
+                }
+
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0: B0 = value; break;
+                    case 1: B1 = value; break;
+                    case 2: B2 = value; break;
+                    case 3: B3 = value; break;
+                    case 4: B4 = value; break;
+                    case 5: B5 = value; break;
+                    case 6: B6 = value; break;
+                    case 7: B7 = value; break;
+                }
+
+            }
         }
 
         [FieldOffset(0)]
-        public unsafe fixed byte B[8];
+        private byte B0;
+
+        [FieldOffset(1)]
+        private byte B1;
+
+        [FieldOffset(2)]
+        private byte B2;
+
+        [FieldOffset(3)]
+        private byte B3;
+
+        [FieldOffset(4)]
+        private byte B4;
+
+        [FieldOffset(5)]
+        private byte B5;
+
+        [FieldOffset(6)]
+        private byte B6;
+
+        [FieldOffset(7)]
+        private byte B7;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 8, CharSet = CharSet.Ansi)]
     public struct INT2
     {
-        public unsafe int this[int index]
-        {
-            get => I[index];
-            set => I[index] = value;
-        }
-
         [FieldOffset(0)]
-        public unsafe fixed int I[2];
+        public int I0;
+
+        [FieldOffset(1)]
+        public int I1;
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 16, CharSet = CharSet.Ansi)]
     public struct UINT4
     {
-        [FieldOffset(0)] /*[MarshalAs(UnmanagedType.LPArray, SizeConst=4)]*/
-        public unsafe fixed uint UI[4];
+        [FieldOffset(0)]
+        public uint UI0;
+        
+        [FieldOffset(0)]
+        public uint UI1;
+        
+        [FieldOffset(0)]
+        public uint UI2;
+
+        [FieldOffset(0)]
+        public uint UI3;
     }
 }
