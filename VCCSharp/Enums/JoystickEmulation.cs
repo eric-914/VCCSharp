d11d0e1c19b65f3cb0db0bf669a2347b780580a3
Eric Hood <eric.hood@ansira.com>
2022-03-27 14:23:51 -0500

    Working on reorganizing the Joystick configuration tab layout so I have room to bring in the joystick 'tester' view.

--- VCCSharp/Enums/JoystickEmulation.cs
+++ VCCSharp/Enums/JoystickEmulation.cs
@@ -1,13 +1,9 @@
-﻿namespace VCCSharp.Enums
+﻿namespace VCCSharp.Enums;
+
+public enum JoystickEmulations
 {
-    // ReSharper disable IdentifierTypo
-    // ReSharper disable InconsistentNaming
-    public enum JoystickEmulations
-    {
-        Standard = 0,
-        TandyHiRes = 1,
-        CCMAX = 2
-    }
-    // ReSharper restore InconsistentNaming
-    // ReSharper restore IdentifierTypo
+    Standard = 0,
+    TandyHiRes = 1,
+    CoCoMaxMouse = 2,
+    XPad = 3
 }
