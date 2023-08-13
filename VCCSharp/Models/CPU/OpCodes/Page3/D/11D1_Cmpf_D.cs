73cd5d6f5e6253cc8ba6f0e430e05b00a46d9374
Eric Hood <eric.hood@ansira.com>
2023-08-13 17:39:27 -0500

    More Page3 opcodes

---
+++ VCCSharp/Models/CPU/OpCodes/Page3/D/11D1_Cmpf_D.cs
@@ -0,0 +1,24 @@
+﻿using VCCSharp.Models.CPU.HD6309;
+using VCCSharp.Models.CPU.MC6809;
+
+namespace VCCSharp.Models.CPU.OpCodes.Page3.D
+{
+    public class _11D1_Cmpf_D : OpCode, IOpCode
+    {
+        public int Exec(IMC6809 cpu) => throw new NotImplementedException();
+
+        public int Exec(IHD6309 cpu)
+        {
+            ushort address = cpu.DPADDRESS(cpu.PC_REG++);
+            byte value = cpu.MemRead8(address);
+            byte compare = (byte)(cpu.F_REG - value);
+
+            cpu.CC_C = compare > cpu.F_REG;
+            cpu.CC_V = OVERFLOW8(cpu.CC_C, value, compare, cpu.F_REG);
+            cpu.CC_N = NTEST8(compare);
+            cpu.CC_Z = ZTEST(compare);
+
+            return Cycles._54;
+        }
+    }
+}
---
+++ VCCSharp/Models/CPU/OpCodes/Page3/D/11D6_Ldf_D.cs
@@ -0,0 +1,23 @@
+﻿using VCCSharp.Models.CPU.HD6309;
+using VCCSharp.Models.CPU.MC6809;
+
+namespace VCCSharp.Models.CPU.OpCodes.Page3.D
+{
+    public class _11D6_Ldf_D : OpCode, IOpCode
+    {
+        public int Exec(IMC6809 cpu) => throw new NotImplementedException();
+
+        public int Exec(IHD6309 cpu)
+        {
+            ushort address = cpu.DPADDRESS(cpu.PC_REG++);
+
+            cpu.F_REG = cpu.MemRead8(address);
+
+            cpu.CC_Z = ZTEST(cpu.F_REG);
+            cpu.CC_N = NTEST8(cpu.F_REG);
+            cpu.CC_V = false;
+
+            return Cycles._54;
+        }
+    }
+}
