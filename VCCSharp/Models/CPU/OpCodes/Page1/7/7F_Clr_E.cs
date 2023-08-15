08d09fed164a1fbe859814697b8f2b64a933970f
Eric Hood <eric.hood@ansira.com>
2023-08-15 18:28:59 -0500

    More opcode detailed documentation

--- VCCSharp/Models/CPU/OpCodes/Page1/7/7F_Clr_E.cs
+++ VCCSharp/Models/CPU/OpCodes/Page1/7/7F_Clr_E.cs
@@ -3,9 +3,42 @@ using VCCSharp.Models.CPU.MC6809;
 
 namespace VCCSharp.Models.CPU.OpCodes.Page1
 {
-    //CLR
-    //Clear accumulator or memory location
-    //EXTENDED
+    /// <summary>
+    /// CLR
+    /// Clear accumulator or memory location
+    /// Store Zero into a Memory Byte
+    /// EXTENDED
+    /// (M) ← 0
+    /// SOURCE FORM     ADDRESSING MODE     OPCODE      CYCLES      BYTE COUNT
+    /// CLR             EXTENDED            7F          7 / 6       3
+    ///   [E F H I N Z V C]
+    ///   [        0 1 0 0]
+    /// </summary>
+    /// <remarks>
+    /// This instruction clears (sets to zero) the byte in memory at the Effective Address specified by the operand. 
+    /// The Condition Code flags are also modified as follows:
+    ///         N The Negative flag is cleared.
+    ///         Z The Zero flag is set.
+    ///         V The Overflow flag is cleared.
+    ///         C The Carry flag is cleared.
+    ///         
+    /// The CPU performs a Read-Modify-Write sequence when this instruction is executed and is therefore slower than an instruction which only writes to memory. 
+    /// When more than one byte needs to be cleared, you can optimize for speed by first clearing an accumulator and then using ST instructions to clear the memory bytes. 
+    /// The following examples illustrate this optimization:
+    /// 
+    /// Executes in 21 cycles (NM=0):
+    ///         CLR $200 ; 7 cycles
+    ///         CLR $210 ; 7 cycles
+    ///         CLR $220 ; 7 cycles
+    ///         
+    /// Adds one additional code byte, but saves 4 cycles:
+    ///         CLRA ; 2 cycles
+    ///         STA $200 ; 5 cycles
+    ///         STA $210 ; 5 cycles
+    ///         STA $220 ; 5 cycles
+    ///         
+    /// See Also: CLR (accumulator), ST
+    /// </remarks>
     public class _7F_Clr_E : OpCode, IOpCode
     {
         private static int Exec(ICpuProcessor cpu, int cycles)
--- VCCSharp/Models/CPU/OpCodes/Page1/9/91_Cmpa_D.cs
+++ VCCSharp/Models/CPU/OpCodes/Page1/9/91_Cmpa_D.cs
@@ -3,9 +3,33 @@ using VCCSharp.Models.CPU.MC6809;
 
 namespace VCCSharp.Models.CPU.OpCodes.Page1
 {
-    //CMPA
-    //Compare memory from accumulator 
-    //DIRECT
+    /// <summary>
+    /// CMPA
+    /// Compare memory from accumulator 
+    /// Compare Memory Byte from 8-Bit Accumulator
+    /// DIRECT
+    /// TEMP ← r - (M)
+    /// SOURCE FORM     ADDRESSING MODE     OPCODE      CYCLES      BYTE COUNT
+    /// CMPA            DIRECT              91          4 / 3       2 
+    ///   [E F H I N Z V C]
+    ///   [    ~   ↕ ↕ ↕ ↕]
+    /// </summary>
+    /// <remarks>
+    /// These instructions subtract the contents of a byte in memory from the value contained in one of the 8-bit accumulators (A,B,E,F) and set the Condition Codes accordingly.
+    /// Neither the memory byte nor the accumulator are modified.
+    ///         H The affect on the Half-Carry flag is undefined for these instructions.
+    ///         N The Negative flag is set equal to the value of bit 7 of the result.
+    ///         Z The Zero flag is set if the resulting value is zero; cleared otherwise.
+    ///         V The Overflow flag is set if an overflow occurred; cleared otherwise.
+    ///         C The Carry flag is set if a borrow into bit-7 was needed; cleared otherwise.
+    /// 
+    /// The Compare instructions are usually used to set the Condition Code flags prior to executing a conditional branch instruction.
+    /// 
+    /// The 8-bit CMP instructions perform exactly the same operation as the 8-bit SUB instructions, with the exception that the value in the accumulator is not changed. 
+    /// Note that since a subtraction is performed, the Carry flag actually represents a Borrow.
+    /// 
+    /// See Also: CMP (16-bit), CMPR
+    /// </remarks>
     public class _91_Cmpa_D : OpCode, IOpCode
     {
         private static int Exec(ICpuProcessor cpu, int cycles)
