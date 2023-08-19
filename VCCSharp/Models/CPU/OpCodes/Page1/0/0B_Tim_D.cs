4cce3ed4dd8cc98b1cb48f65b0f5517634502a4b
Eric Hood <eric.hood@ansira.com>
2023-08-19 10:02:05 -0500

    Finally finished the opcode detailed documentation transcribing.  Source: https://thezippsterzone.com/wp-content/uploads/2018/06/6x09_Instruction_Sets.pdf

--- VCCSharp/Models/CPU/OpCodes/Page1/0/0B_Tim_D.cs
+++ VCCSharp/Models/CPU/OpCodes/Page1/0/0B_Tim_D.cs
@@ -3,9 +3,46 @@ using VCCSharp.Models.CPU.MC6809;
 
 namespace VCCSharp.Models.CPU.OpCodes.Page1
 {
-    // --[HITACHI]--
-    //TIM
-    //DIRECT
+    /// <summary>
+    /// TIM
+    /// 🚫 6309 ONLY 🚫
+    /// Bit Test Immediate Value with Memory Byte
+    /// DIRECT
+    /// TEMP ← (M) AND IMM8
+    /// SOURCE FORM     ADDRESSING MODE     OPCODE      CYCLES      BYTE COUNT
+    /// TIM #i8;EA      DIRECT              0B          6           3 
+    ///                 
+    /// I8 : 8-bit Immediate value
+    ///   [E F H I N Z V C]
+    ///   [        ↕ ↕ 0  ]
+    /// </summary>
+    /// <remarks>
+    /// The TIM instruction logically ANDs the contents of a byte in memory with an 8-bit immediate value. 
+    /// The resulting value is tested and then discarded. 
+    /// The Condition Codes are updated to reflect the results of the test as follows:
+    ///     N The Negative flag is set equal to bit 7 of the resulting value.
+    ///     Z The Zero flag is set if the resulting value was zero; cleared otherwise.
+    ///     V The Overflow flag is cleared by this instruction.
+    ///     C The Carry flag is not affected by this instruction.
+    ///     
+    /// TIM can be used as a space-saving optimization for a pair of equivalent 6809 instructions, and to perform a bit test without having to utilize a register. 
+    /// However, it is slower than the 6809 equivalent:
+    /// 
+    ///     6809: (4 instruction bytes; 7 cycles):
+    ///         LDA #$3F
+    ///         BITA 4,U
+    ///     6309: (3 instruction bytes; 8 cycles):
+    ///         TIM #$3F;4,U
+    ///         
+    /// Note that the assembler syntax used for the TIM operand is non-typical. 
+    /// Some assemblers may require a comma (,) rather than a semicolon (;) between the immediate operand and the address operand.
+    /// The object code format for the TIM instruction is:
+    ///     ╭────────┬─────────────┬─────────────────────────╮
+    ///     │ OPCODE │ IMMED VALUE │ ADDRESS / INDEX BYTE(S) │
+    ///     ╰────────┴─────────────┴─────────────────────────╯
+    /// 
+    /// See Also: AIM, AND, EIM, OIM
+    /// </remarks>
     public class _0B_Tim_D : OpCode, IOpCode
     {
         public int Exec(IMC6809 cpu)
--- VCCSharp/Models/CPU/OpCodes/Page3/5/115D_Tstf_I.cs
+++ VCCSharp/Models/CPU/OpCodes/Page3/5/115D_Tstf_I.cs
@@ -3,10 +3,43 @@ using VCCSharp.Models.CPU.MC6809;
 
 namespace VCCSharp.Models.CPU.OpCodes.Page3
 {
-    // --[HITACHI]--
-    //TSTF
-    //INHERENT
-    public class _115D_Tstf_I : OpCode, IOpCode
+    /// <summary>
+    /// TSTF
+    /// 🚫 6309 ONLY 🚫
+    /// Test accumulator or memory location 
+    /// Test Value in Accumulator
+    /// INHERENT
+    /// TEMP ← r
+    /// SOURCE FORM     ADDRESSING MODE     OPCODE       CYCLES     BYTE COUNT
+    /// TSTD            INHERENT            104D         3 / 2      2
+    /// TSTE            INHERENT            114D         3 / 2      2
+    /// TSTF            INHERENT            115D         3 / 2      2
+    /// TSTW            INHERENT            105D         3 / 2      2
+    ///   [E F H I N Z V C]
+    ///   [        ↕ ↕ 0  ]
+    /// </summary>
+    /// <remarks>
+    /// The TST instructions test the value in an accumulator to setup the Condition Codes register with minimal status for that value. 
+    /// The accumulator itself is not modified by these instructions.
+    ///         N The Negative flag is set equal to the value of the accumulator’s high-order bit (sign bit).
+    ///         Z The Zero flag is set if the accumulator’s value is zero; cleared otherwise.
+    ///         V The Overflow flag is always cleared.
+    ///         C The Carry flag is not affected by these instructions.
+    ///         
+    /// For unsigned values, the only meaningful information provided is whether or not the value is zero. 
+    /// In this case, BEQ or BNE would typically follow such a test. 
+    /// 
+    /// For signed (twos complement) values, the information provided is sufficient to allow any of the signed conditional branches (BGE, BGT, BLE, BLT) to be used as though the accumulator’s value had been compared with zero. 
+    /// You can also use BMI and BPL to branch according to the sign of the value.
+    /// 
+    /// To determine the sign of a 16-bit or 32-bit value, you only need to test the high order byte. 
+    /// For example, TSTA is sufficient for determining the sign of a 32-bit twos complement value in accumulator Q. 
+    /// A full test of accumulator Q could be accomplished by storing it to a scratchpad RAM location (or ROM address). 
+    /// In a traditional stack environment, the instruction STQ -4,S may be acceptable.
+    /// 
+    /// See Also: CMP, STQ, TST (memory)
+    /// </remarks>
+   public class _115D_Tstf_I : OpCode, IOpCode
     {
         public int Exec(IMC6809 cpu) => throw new NotImplementedException();
 
