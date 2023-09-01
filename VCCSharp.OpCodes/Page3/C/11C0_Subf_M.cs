b336d23632591032d020cb52f19b7b5ac4aaafb1
Eric Hood <eric.hood@ansira.com>
2023-08-31 21:15:57 -0500

    More opcodes:Page3.Cx

---
+++ VCCSharp.OpCodes/Page3/C/11C0_Subf_M.cs
@@ -0,0 +1,53 @@
+﻿using VCCSharp.OpCodes.Model.OpCodes;
+
+namespace VCCSharp.OpCodes.Page3;
+
+/// <summary>
+/// <code>11C0/SUBF/IMMEDIATE</code>
+/// Subtract from value in 8-Bit Accumulator <c>F</c>
+/// <code>F’ ← F - IMM8|(M)</code>
+/// </summary>
+/// <remarks>
+/// The <c>SUBF</c> instruction subtracts an 8-bit immediate value from one of the 8-bit accumulator <c>F</c>.
+/// The 8-bit result is placed back into the specified accumulator
+/// <code>🚫 6309 ONLY 🚫</code>
+/// </remarks>
+/// 
+/// [E F H I N Z V C]
+/// [    ~   ↕ ↕ ↕ ↕]
+/// 
+/// Note that since subtraction is performed, the purpose of the Carry flag is to represent a Borrow.
+///         H The value of Half-Carry flag is undefined after executing these instructions.
+///         N The Negative flag is set equal to the new value of bit 7 of the accumulator.
+///         Z The Zero flag is set if the new accumulator value is zero; cleared otherwise.
+///         V The Overflow flag is set if an overflow occurred; cleared otherwise.
+///         C The Carry flag is set if a borrow into bit 7 was needed; cleared otherwise.
+///         
+/// The 8-bit SUB instructions are used for single-byte subtraction, and for subtraction of the least-significant byte in multi-byte subtractions.
+/// Since the 6809 and 6309 both provide 16-bit SUB instructions for the accumulators, it is not necessary to use the 8-bit SUB and SBC instructions to perform 16-bit subtraction.
+/// 
+/// Cycles (3)
+/// Byte Count (3)
+/// 
+/// See Also: SUB (16-bit), SUBR
+internal class _11C0_Subf_M : OpCode6309, IOpCode
+{
+    internal _11C0_Subf_M(HD6309.IState cpu) : base(cpu) { }
+
+    public int Exec()
+    {
+        byte value = M8[PC++];
+
+        var sum = Subtract(F, value);
+
+        CC_H = sum.H;
+        CC_N = sum.N;
+        CC_Z = sum.Z;
+        CC_V = sum.V;
+        CC_C = sum.C;
+
+        F = (byte)sum.Result;
+
+        return 3;
+    }
+}
---
+++ VCCSharp.OpCodes/Page3/C/11CB_Addf_M.cs
@@ -0,0 +1,52 @@
+﻿using VCCSharp.OpCodes.Model.OpCodes;
+
+namespace VCCSharp.OpCodes.Page3;
+
+/// <summary>
+/// <code>11BB/ADDE/IMMEDIATE</code>
+/// Add Memory Byte to 8-Bit Accumulator <c>F</c>
+/// <code>F’ ← F + (M)</code>
+/// </summary>
+/// <remarks>
+/// The <c>ADDF</c> instruction adds the contents of a byte in memory with the 8-bit <c>F</c> accumulator.
+/// The 8-bit result is placed back into the <c>F</c> accumulator.
+/// <code>🚫 6309 ONLY 🚫</code>
+/// </remarks>
+/// 
+/// [E F H I N Z V C]
+/// [    ↕   ↕ ↕ ↕ ↕]
+/// 
+///         H The Half-Carry flag is set if a carry into bit 4 occurred; cleared otherwise.
+///         N The Negative flag is set equal to the new value of bit 7 of the accumulator.
+///         Z The Zero flag is set if the new accumulator value is zero; cleared otherwise.
+///         V The Overflow flag is set if an overflow occurred; cleared otherwise.
+///         C The Carry flag is set if a carry out of bit 7 occurred; cleared otherwise.
+///         
+/// The 8-bit ADD instructions are used for single-byte addition, and for addition of the least-significant byte in multi-byte additions. 
+/// Since the 6x09 also provides a 16-bit ADD instruction, it is not necessary to use the 8-bit ADD and ADC instructions for performing 16-bit addition.
+/// 
+/// Cycles (3)
+/// Byte Count (3)
+/// 
+/// See Also: ADD (16-bit), ADDR
+internal class _11CB_Addf_M : OpCode6309, IOpCode
+{
+    internal _11CB_Addf_M(HD6309.IState cpu) : base(cpu) { }
+
+    public int Exec()
+    {
+        byte value = M8[PC++];
+
+        var sum = Add(F, value);
+
+        CC_H = sum.H;
+        CC_N = sum.N;
+        CC_Z = sum.Z;
+        CC_V = sum.V;
+        CC_C = sum.C;
+
+        F = (byte)sum.Result;
+
+        return 3;
+    }
+}
